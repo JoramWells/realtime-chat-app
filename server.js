87c438e5a48e3c08a4f9e9f339f97fe4c3cbe047
bramuel <jorammanoah1@gmail.com>
2021-02-18 09:58:01 -0800

    Public commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const path = require('path')
+
+
 const app = express();
 
+app.use(express.static(path.__dirname, 'public'))
+
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on http://localhost:3000/"))
 
